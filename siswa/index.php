87268b06eea61ad8872f60ff1da0465c1cced199
siwa <siwarjsa@gmail.com>
2022-02-12 14:41:01 +0800

    update

---
+++ siswa/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../templates/header.php';
+?>
+
+<div class="container">
+    <h1>Hai</h1>
+</div>
+<?php
+require_once '../templates/footer.php';
+?>
--- templates/header.php
+++ templates/header.php
@@ -12,10 +12,9 @@ $spp = mysqli_query($koneksi, "SELECT * FROM spp");
 
 <head>
     <title></title>
-    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link rel="stylesheet" href="../templates/css/style.css" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-
 
 <body>
 

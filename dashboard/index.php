0a45071ba69dc38d18d5eac51ed78efb60d0d589
siwa <siwarjsa@gmail.com>
2022-02-12 14:22:51 +0800

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,107 +1,34 @@
 <?php
-include('../koneksi.php');
-session_start();
-$kelas = mysqli_query($koneksi, "SELECT * FROM kelas");
-$pembayaran = mysqli_query($koneksi, "SELECT * FROM pembayaran");
-$petugas = mysqli_query($koneksi, "SELECT * FROM petugas");
-$siswa = mysqli_query($koneksi, "SELECT * FROM siswa");
-$spp = mysqli_query($koneksi, "SELECT * FROM spp");
+require_once '../templates/header.php';
 ?>
+<!-- content -->
 
-<html>
-
-<head>
-    <title></title>
-    <link rel="stylesheet" href="css/style.css" type="text/css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-
-<body>
-
-    <div id="mySidenav" class="sidenav">
-        <p class="logo"><img src="https://elearning.smkti-baliglobal.sch.id/img/logo-ti2.png" width="130px" height="130px" alt=""></p>
-        <a href="#" class="icon-a"><i class="fa fa-dashboard icons"></i> &nbsp;&nbsp;Dashboard</a>
-        <a href="#" class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Kelas</a>
-        <a href="#" class="icon-a"><i class="fa fa-shopping-bag icons"></i> &nbsp;&nbsp;Pembayaran</a>
-        <a href="#" class="icon-a"><i class="fa fa-list icons"></i> &nbsp;&nbsp;Petugas</a>
-        <a href="#" class="icon-a"><i class="fa fa-tasks icons"></i> &nbsp;&nbsp;Siswa</a>
-        <a href="#" class="icon-a"><i class="fa fa-list-alt icons"></i> &nbsp;&nbsp;History</a>
-        <a href="../logout.php" class="icon-a"><i class="fa fa-sign-out icons"></i> &nbsp;&nbsp;Logout</a>
+<div class="col-div-3">
+    <div class="box">
+        <p><?= mysqli_num_rows($kelas) ?><br /><span>Kelas</span></p>
+        <i class="fa fa-users box-icon"></i>
     </div>
-    <div id="main">
-        <div class="head">
-            <div class="col-div-6">
-                <span style="font-size:30px;cursor:pointer; color: white;" class="nav">&#9776; Dashboard</span>
-                <span style="font-size:30px;cursor:pointer; color: white;" class="nav2">&#9776; Dashboard</span>
-            </div>
-
-            <div class="col-div-6">
-                <div class="profile">
-
-                    <img src="images/user.png" class="pro-img" />
-                    <p>Manoj Adhikari <span>UI / UX DESIGNER</span></p>
-                </div>
-            </div>
-            <div class="clearfix"></div>
-        </div>
-
-        <div class="clearfix"></div>
-        <br />
-
-        <div class="col-div-3">
-            <div class="box">
-                <p><?= mysqli_num_rows($kelas) ?><br /><span>Kelas</span></p>
-                <i class="fa fa-users box-icon"></i>
-            </div>
-        </div>
-        <div class="col-div-3">
-            <div class="box">
-                <p><?= mysqli_num_rows($petugas) ?><br /><span>Petugas</span></p>
-                <i class="fa fa-list box-icon"></i>
-            </div>
-        </div>
-        <div class="col-div-3">
-            <div class="box">
-                <p><?= mysqli_num_rows($siswa) ?><br /><span>Siswa</span></p>
-                <i class="fa fa-tasks box-icon"></i>
-            </div>
-        </div>
-        <div class="col-div-3">
-            <div class="box">
-                <p>0<br /><span>History</span></p>
-                <i class="fa fa-list-alt box-icon"></i>
-            </div>
-        </div>
+</div>
+<div class="col-div-3">
+    <div class="box">
+        <p><?= mysqli_num_rows($petugas) ?><br /><span>Petugas</span></p>
+        <i class="fa fa-list box-icon"></i>
     </div>
+</div>
+<div class="col-div-3">
+    <div class="box">
+        <p><?= mysqli_num_rows($siswa) ?><br /><span>Siswa</span></p>
+        <i class="fa fa-tasks box-icon"></i>
+    </div>
+</div>
+<div class="col-div-3">
+    <div class="box">
+        <p>0<br /><span>History</span></p>
+        <i class="fa fa-list-alt box-icon"></i>
+    </div>
+</div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script>
-        $(".nav").click(function() {
-            $("#mySidenav").css('width', '70px');
-            $("#main").css('margin-left', '70px');
-            $(".logo").css('visibility', 'hidden');
-            $(".logo span").css('visibility', 'visible');
-            $(".logo span").css('margin-left', '-10px');
-            $(".icon-a").css('visibility', 'hidden');
-            $(".icons").css('visibility', 'visible');
-            $(".icons").css('margin-left', '-8px');
-            $(".nav").css('display', 'none');
-            $(".nav2").css('display', 'block');
-        });
-
-        $(".nav2").click(function() {
-            $("#mySidenav").css('width', '300px');
-            $("#main").css('margin-left', '300px');
-            $(".logo").css('visibility', 'visible');
-            $(".icon-a").css('visibility', 'visible');
-            $(".icons").css('visibility', 'visible');
-            $(".nav").css('display', 'block');
-            $(".nav2").css('display', 'none');
-        });
-    </script>
-
-</body>
-
-
-</html>
+<!-- end content -->
+<?php
+require_once '../templates/footer.php';
+?>
---
+++ templates/header.php
@@ -0,0 +1,50 @@
+<?php
+include('../koneksi.php');
+session_start();
+$kelas = mysqli_query($koneksi, "SELECT * FROM kelas");
+$pembayaran = mysqli_query($koneksi, "SELECT * FROM pembayaran");
+$petugas = mysqli_query($koneksi, "SELECT * FROM petugas");
+$siswa = mysqli_query($koneksi, "SELECT * FROM siswa");
+$spp = mysqli_query($koneksi, "SELECT * FROM spp");
+?>
+
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+
+<body>
+
+    <div id="mySidenav" class="sidenav">
+        <p class="logo"><img src="https://elearning.smkti-baliglobal.sch.id/img/logo-ti2.png" width="130px" height="130px" alt=""></p>
+        <a href="#" class="icon-a"><i class="fa fa-dashboard icons"></i> &nbsp;&nbsp;Dashboard</a>
+        <a href="#" class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Kelas</a>
+        <a href="#" class="icon-a"><i class="fa fa-shopping-bag icons"></i> &nbsp;&nbsp;Pembayaran</a>
+        <a href="#" class="icon-a"><i class="fa fa-list icons"></i> &nbsp;&nbsp;Petugas</a>
+        <a href="#" class="icon-a"><i class="fa fa-tasks icons"></i> &nbsp;&nbsp;Siswa</a>
+        <a href="#" class="icon-a"><i class="fa fa-list-alt icons"></i> &nbsp;&nbsp;History</a>
+        <a href="../logout.php" class="icon-a"><i class="fa fa-sign-out icons"></i> &nbsp;&nbsp;Logout</a>
+    </div>
+    <div id="main">
+        <div class="head">
+            <div class="col-div-6">
+                <span style="font-size:30px;cursor:pointer; color: white;" class="nav">&#9776; Dashboard</span>
+                <span style="font-size:30px;cursor:pointer; color: white;" class="nav2">&#9776; Dashboard</span>
+            </div>
+
+            <div class="col-div-6">
+                <div class="profile">
+
+                    <img src="images/user.png" class="pro-img" />
+                    <p>Siwa <span>UI / UX DESIGNER</span></p>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+
+        <div class="clearfix"></div>
+        <br />

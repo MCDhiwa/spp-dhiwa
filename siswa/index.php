e8018641863a0a82bcbe5899630c4001176e87c7
siwa <siwarjsa@gmail.com>
2022-02-16 12:09:53 +0800

    update

--- siswa/index.php
+++ siswa/index.php
@@ -5,6 +5,7 @@ require_once '../templates/header.php';
 <div class="container">
     <h1>Hai</h1>
 </div>
+
 <?php
 require_once '../templates/footer.php';
 ?>
--- templates/footer.php
+++ templates/footer.php
@@ -15,6 +15,20 @@
         $(".nav2").css('display', 'block');
     });
 
+    // const nav = document.querySelector('.nav');
+    // nav.addEventListener('click', () => {
+    //     document.querySelector('#mySidenav').setAttribute('style', 'width: 70px');
+    //     document.querySelector('#main').setAttribute('style', 'margin-left: 70px');
+    //     document.querySelector('.logo').setAttribute('style', 'visibility: hidden');
+    //     document.querySelector('.logo span').setAttribute('style', 'visibility: hidden');
+    //     document.querySelector('.logo span').setAttribute('style', 'margin-left: -10px');
+    //     document.querySelector('.icon-a').setAttribute('style', 'visibility: hidden');
+    //     document.querySelector('.icons').setAttribute('style', 'visibility: visible');
+    //     document.querySelector('.icons').setAttribute('style', 'margin-left: -8px');
+    //     document.querySelector('.nav').setAttribute('style', 'display: none');
+    //     document.querySelector('.nav2').setAttribute('style', 'display: block');
+    // });
+
     $(".nav2").click(function() {
         $("#mySidenav").css('width', '300px');
         $("#main").css('margin-left', '300px');
@@ -24,6 +38,17 @@
         $(".nav").css('display', 'block');
         $(".nav2").css('display', 'none');
     });
+
+    // const nav2 = document.querySelector('.nav2');
+    // nav2.addEventListener('click', () => {
+    //     document.querySelector('#mySidenav').setAttribute('width: 300px');
+    //     document.querySelector('#main').setAttribute('margin-left: 300px');
+    //     document.querySelector('.logo').setAttribute('visibility: visible');
+    //     document.querySelector('.icon-a').setAttribute('visibility: visible');
+    //     document.querySelector('.icons').setAttribute('visibility: visible');
+    //     document.querySelector('.nav').setAttribute('display: block');
+    //     document.querySelector('.nav2').setAttribute('display: none');
+    // });
 </script>
 
 </body>
